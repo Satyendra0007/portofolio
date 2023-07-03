7932753f0dd58f3ddadc2728e1cdbe6cc8b4e9f0
satyendra <skchandrawansi03@gmail.com>
2023-07-03 20:19:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+try {
+  var typed = new Typed('#element', {
+    strings: ['Programmer. ', 'Student. ', 'Front-End Developer. '],
+    typeSpeed: 70,
+  });
+
+} catch {
+
+}
+try {
+  document.getElementById('Github').onclick = () => {
+    window.location.href = "https://github.com/Satyendra0007";
+  }
+} catch (error) {
+
+}
+
+try {
+  document.getElementById('Replit').onclick = () => {
+    window.location.href = "https://replit.com/@Satyendra03";
+  }
+} catch (error) {
+
+}
+
+
+
+
