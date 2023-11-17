0ca2a7aa63332193be5e0019289466a2788f2fd0
satyendra <skchandrawansi03@gmail.com>
2023-11-17 09:01:42 +0530

    script file added

---
+++ dist/assets/script-4750a857.js
@@ -0,0 +1,1 @@
+try{var e=new Typed("#element",{strings:["Programmer. ","Student. ","Front-End Developer. "],typeSpeed:70})}catch{}try{document.getElementById("Github").onclick=()=>{window.location.href="https://github.com/Satyendra0007"}}catch{}try{document.getElementById("Replit").onclick=()=>{window.location.href="https://replit.com/@Satyendra03"}}catch{}
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
         main: './index.html',
         about: './about.html',
         project: './project.html',
-        script: 'script.js'
+        script: './script.js'
         // ...
         // List all files you want in your build
       }
